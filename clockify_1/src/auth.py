c2ed1288ad93cc04ed0a5acc8bea76c1fc80687b
AlexVlad <navigatort@yandex.ru>
2020-11-28 19:05:57 +0300

    Тестовое задание v1.1

--- clockify_1/src/auth.py
+++
@@ -1,9 +0,0 @@
-import requests
-21
-session = requests.Session()
-session.post('https://clockify.me/login', {
-     'username': 'pbobchinsky@yandex.ru',
-     'password': 'AlexVlad_clockify',
-     'remember': 1,
-})
-
